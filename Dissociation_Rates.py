968be4565cb2240f63488d70cad6e95f85c2841a
EmicoBinsfinder <109655670+EmicoBinsfinder@users.noreply.github.com>
2022-11-22 15:21:59 +0000

    Update Dissociation_Rates.py

--- Dissociation_Rates.py
+++ Dissociation_Rates.py
@@ -1,37 +1,130 @@
-Dissociation_Rate_1ms_1GPa_Coefficients = [-0.06826, -0.06975, -0.06678]
-Dissociation_Rate_1ms_2GPa_Coefficients = [-0.4788, -0.4847, -0.4729]
-Dissociation_Rate_1ms_3GPa_Coefficients = [-0.8493, -0.8653, -0.8334]
-Dissociation_Rate_1ms_4GPa_Coefficients = [-1.359, -1.388, -1.33]
-Dissociation_Rate_1ms_5GPa_Coefficients = [-1.479, -1.515, -1.442]
 
-Dissociation_Rate_10ms_1GPa_Coefficients = [-1.244, -1.267, -1.221]
-Dissociation_Rate_10ms_2GPa_Coefficients = [-2.761, -2.806, -2.716]
-Dissociation_Rate_10ms_3GPa_Coefficients = [-4.645, -4.68, -4.609]
-Dissociation_Rate_10ms_4GPa_Coefficients = [-4.825, -4.9, -4.75]
-Dissociation_Rate_10ms_5GPa_Coefficients = [-7.974, -8.198, -7.749]
+### 10ms Comparison
+Dissociation_Rate_10ms_400K_1GPa_Coefficients = [-1.312, -1.332, -1.292]
+Dissociation_Rate_10ms_400K_2GPa_Coefficients = [-2.816, -2.857, -2.774]
+Dissociation_Rate_10ms_400K_3GPa_Coefficients = [-4.637, -4.67, -4.605]
+Dissociation_Rate_10ms_400K_4GPa_Coefficients = [-4.821, -4.889, -4.754]
+Dissociation_Rate_10ms_400K_5GPa_Coefficients = [-7.973, -8.176, -7.771]
 
-Dissociation_Rate_20ms_1GPa_Coefficients = [-1.854, -1.884, -1.824]
-Dissociation_Rate_20ms_2GPa_Coefficients = [-4.373, -4.437, -4.31]
-Dissociation_Rate_20ms_3GPa_Coefficients = [-5.679, -5.733, -5.624]
-Dissociation_Rate_20ms_4GPa_Coefficients = [-7.832, -7.946, -7.719]
-Dissociation_Rate_20ms_5GPa_Coefficients = [-10.58, -10.73, -10.44]
+Dissociation_Rate_10ms_500K_1GPa_Coefficients = [-2.566, -2.596, -2.536]
+Dissociation_Rate_10ms_500K_2GPa_Coefficients = [-3.667, -3.696, -3.639]
+Dissociation_Rate_10ms_500K_3GPa_Coefficients = [-5.698, -5.769, -5.627]
+Dissociation_Rate_10ms_500K_4GPa_Coefficients = [-5.741, -5.84, -5.642]
+Dissociation_Rate_10ms_500K_5GPa_Coefficients = [-8.577, -8.804, -8.35]
 
-Dissociation_Rate_30ms_1GPa_Coefficients = [-2.489, -2.521, -2.457]
-Dissociation_Rate_30ms_2GPa_Coefficients = [-5.954, -6.068, -5.839]
-Dissociation_Rate_30ms_3GPa_Coefficients = [-10.38, -10.58, -10.19]
-Dissociation_Rate_30ms_4GPa_Coefficients = [-11.41, -11.55, -11.28]
-Dissociation_Rate_30ms_5GPa_Coefficients = [-18.21, -18.53, -17.89]
+Dissociation_Rate_10ms_600K_1GPa_Coefficients = [-4.254, -4.312, -4.196]
+Dissociation_Rate_10ms_600K_2GPa_Coefficients = [-5.646, -5.688, -5.604]
+Dissociation_Rate_10ms_600K_3GPa_Coefficients = [-7.889, -8.026, -7.751]
+Dissociation_Rate_10ms_600K_4GPa_Coefficients = [-9.025, -9.255, -8.795]
+Dissociation_Rate_10ms_600K_5GPa_Coefficients = [-17.94, -18.47, -17.4]
 
-Dissociation_Rate_40ms_1GPa_Coefficients = [-4.357, -4.4, -4.315]
-Dissociation_Rate_40ms_2GPa_Coefficients = [-7.186, -7.267, -7.104]
-Dissociation_Rate_40ms_3GPa_Coefficients = [-9.288, -9.433, -9.142]
-Dissociation_Rate_40ms_4GPa_Coefficients = [-15.24, -15.4, -15.08]
-Dissociation_Rate_40ms_5GPa_Coefficients = [-17.83, -17.97, -17.69]
+Dissociation_Rate_10ms_700K_1GPa_Coefficients = [-5.19, -5.238, -5.142]
+Dissociation_Rate_10ms_700K_2GPa_Coefficients = [-9.312, -9.374, -9.25]
+Dissociation_Rate_10ms_700K_3GPa_Coefficients = [-13.85, -14.03, -13.67]
+Dissociation_Rate_10ms_700K_4GPa_Coefficients = [-19.61, -20.07, -19.15]
+Dissociation_Rate_10ms_700K_5GPa_Coefficients = [-26.73, -27.62, -25.84]
 
-Dissociation_Rate_50ms_1GPa_Coefficients = [-6.189, -6.24, -6.138]
-Dissociation_Rate_50ms_2GPa_Coefficients = [-9.588, -9.763, -9.413]
-Dissociation_Rate_50ms_3GPa_Coefficients = [-14.35, -14.59, -14.1]
-Dissociation_Rate_50ms_4GPa_Coefficients = [-18.29, -18.45, -18.13]
-Dissociation_Rate_50ms_5GPa_Coefficients = [-23.16, -23.49, -22.83]
+### 20ms
+Dissociation_Rate_20ms_400K_1GPa_Coefficients = [-1.874, -1.902, -1.845]
+Dissociation_Rate_20ms_400K_2GPa_Coefficients = [-5.243, -5.312, -5.175]
+Dissociation_Rate_20ms_400K_3GPa_Coefficients = [-8.289, -8.349, -8.229]
+Dissociation_Rate_20ms_400K_4GPa_Coefficients = [-10.78, -10.94, -10.63]
+Dissociation_Rate_20ms_400K_5GPa_Coefficients = [-10.58, -10.72, -10.45]
 
-Rates_List = dir()
+Dissociation_Rate_20ms_500K_1GPa_Coefficients = [-3.018, -3.033, -3.003]
+Dissociation_Rate_20ms_500K_2GPa_Coefficients = [-6.581, -6.64, -6.522]
+Dissociation_Rate_20ms_500K_3GPa_Coefficients = [-10.65, -10.74, -10.56]
+Dissociation_Rate_20ms_500K_4GPa_Coefficients = [-11.14, -11.28, -11]
+Dissociation_Rate_20ms_500K_5GPa_Coefficients = [-21.25, -21.79, -20.7]
+
+Dissociation_Rate_20ms_600K_1GPa_Coefficients = [-6.11, -6.15, -6.07]
+Dissociation_Rate_20ms_600K_2GPa_Coefficients = [-9.106, -9.175, -9.037]
+Dissociation_Rate_20ms_600K_3GPa_Coefficients = [-15.52, -15.7, -15.35]
+Dissociation_Rate_20ms_600K_4GPa_Coefficients = [-15.35, -15.49, -15.2]
+Dissociation_Rate_20ms_600K_5GPa_Coefficients = [-24.51, -25.03, -23.98]
+
+Dissociation_Rate_20ms_700K_1GPa_Coefficients = [-9.297, -9.373, -9.221]
+Dissociation_Rate_20ms_700K_2GPa_Coefficients = [-11.97, -12.12, -11.82]
+Dissociation_Rate_20ms_700K_3GPa_Coefficients = [-23.86, -24.24, -23.48]
+Dissociation_Rate_20ms_700K_4GPa_Coefficients = [-23.85, -24.06, -23.64]
+Dissociation_Rate_20ms_700K_5GPa_Coefficients = [-23.58, -24.01, -23.15]
+
+### 30ms
+
+Dissociation_Rate_30ms_400K_1GPa_Coefficients = [-2.773, -2.81, -2.735]
+Dissociation_Rate_30ms_400K_2GPa_Coefficients = [-5.898, -6.013, -5.783]
+Dissociation_Rate_30ms_400K_3GPa_Coefficients = [-10.13, -10.21, -10.05]
+Dissociation_Rate_30ms_400K_4GPa_Coefficients = [-12.74, -12.83, -12.65]
+Dissociation_Rate_30ms_400K_5GPa_Coefficients = [-15.22, -15.36, -15.08]
+
+Dissociation_Rate_30ms_500K_1GPa_Coefficients = [-3.891, -3.956, -3.827]
+Dissociation_Rate_30ms_500K_2GPa_Coefficients = [-9.901, -9.987, -9.814]
+Dissociation_Rate_30ms_500K_3GPa_Coefficients = [-11.18, -11.28, -11.07]
+Dissociation_Rate_30ms_500K_4GPa_Coefficients = [-14.87, -15.03, -14.71]
+Dissociation_Rate_30ms_500K_5GPa_Coefficients = [-27.37, -27.97, -26.77]
+
+Dissociation_Rate_30ms_600K_1GPa_Coefficients = [-6.713, -6.794, -6.631]
+Dissociation_Rate_30ms_600K_2GPa_Coefficients = [-11.75, -11.88, -11.61]
+Dissociation_Rate_30ms_600K_3GPa_Coefficients = [-18.23, -18.42, -18.05]
+Dissociation_Rate_30ms_600K_4GPa_Coefficients = [-18.44, -18.6, -18.28]
+Dissociation_Rate_30ms_600K_5GPa_Coefficients = [-36.05, -36.62, -35.48]
+
+Dissociation_Rate_30ms_700K_1GPa_Coefficients = [-8.398, -8.47, -8.326]
+Dissociation_Rate_30ms_700K_2GPa_Coefficients = [-17.04, -17.27, -16.81]
+Dissociation_Rate_30ms_700K_3GPa_Coefficients = [-23.86, -24.24, -23.48]
+Dissociation_Rate_30ms_700K_4GPa_Coefficients = [-32.19, -32.84, -31.54]
+Dissociation_Rate_30ms_700K_5GPa_Coefficients = [-42.99, -43.45, -42.54]
+
+### 40ms
+
+Dissociation_Rate_40ms_400K_1GPa_Coefficients = [-3.213, -3.262, -3.163]
+Dissociation_Rate_40ms_400K_2GPa_Coefficients = [-7.241, -7.308, -7.173]
+Dissociation_Rate_40ms_400K_3GPa_Coefficients = [-13.88, -13.98, -13.79]
+Dissociation_Rate_40ms_400K_4GPa_Coefficients = [-17.31, -17.53, -17.09]
+Dissociation_Rate_40ms_400K_5GPa_Coefficients = [-25.15, -25.45, -24.85]
+
+Dissociation_Rate_40ms_500K_1GPa_Coefficients = [-4.248, -4.281, -4.214]
+Dissociation_Rate_40ms_500K_2GPa_Coefficients = [-11.2, -11.35, -11.05]
+Dissociation_Rate_40ms_500K_3GPa_Coefficients = [-15.27, -15.43, -15.12]
+Dissociation_Rate_40ms_500K_4GPa_Coefficients = [-25.43, -25.65, -25.2]
+Dissociation_Rate_40ms_500K_5GPa_Coefficients = [-32.91, -33.61, -32.21]
+
+Dissociation_Rate_40ms_600K_1GPa_Coefficients = [-7.993, -8.135, -7.851]
+Dissociation_Rate_40ms_600K_2GPa_Coefficients = [-15.31, -15.46, -15.16]
+Dissociation_Rate_40ms_600K_3GPa_Coefficients = [-20.62, -20.77, -20.47]
+Dissociation_Rate_40ms_600K_4GPa_Coefficients = [-38.54, -38.94, -38.15]
+Dissociation_Rate_40ms_600K_5GPa_Coefficients = [-36.05, -36.62, -35.48]
+
+Dissociation_Rate_40ms_700K_1GPa_Coefficients = [-10.59, -10.72, -10.46]
+Dissociation_Rate_40ms_700K_2GPa_Coefficients = [-17.21, -17.33, -17.1]
+Dissociation_Rate_40ms_700K_3GPa_Coefficients = [-25.42, -25.6, -25.23]
+Dissociation_Rate_40ms_700K_4GPa_Coefficients = [-23.56, -23.78, -23.34]
+Dissociation_Rate_40ms_700K_5GPa_Coefficients = [-41.01, -41.37, -40.64]
+
+### 50ms
+
+Dissociation_Rate_50ms_400K_1GPa_Coefficients = [-4.326, -4.369, -4.283]
+Dissociation_Rate_50ms_400K_2GPa_Coefficients = [-8.726, -8.918, -8.533]
+Dissociation_Rate_50ms_400K_3GPa_Coefficients = [-15.84, -16.06, -15.61]
+Dissociation_Rate_50ms_400K_4GPa_Coefficients = [-26.19, -26.46, -25.92]
+Dissociation_Rate_50ms_400K_5GPa_Coefficients = [-27.72, -28.01, -27.44]
+
+Dissociation_Rate_50ms_500K_1GPa_Coefficients = [-6.477, -6.587, -6.366]
+Dissociation_Rate_50ms_500K_2GPa_Coefficients = [-9.838, -9.994, -9.682]
+Dissociation_Rate_50ms_500K_3GPa_Coefficients = [-23, -23.19, -22.82]
+Dissociation_Rate_50ms_500K_4GPa_Coefficients = [-26.52, -26.73, -26.32]
+Dissociation_Rate_50ms_500K_5GPa_Coefficients = [-35.73, -36.28, -35.18]
+
+Dissociation_Rate_50ms_600K_1GPa_Coefficients = [-6.134, -6.199, -6.068]
+Dissociation_Rate_50ms_600K_2GPa_Coefficients = [-15.08, -15.23, -14.93]
+Dissociation_Rate_50ms_600K_3GPa_Coefficients = [-24.59, -24.8, -24.38]
+Dissociation_Rate_50ms_600K_4GPa_Coefficients = [-23.13, -23.34, -22.91]
+Dissociation_Rate_50ms_600K_5GPa_Coefficients = [-42.71, -43.12, -42.3]
+
+Dissociation_Rate_50ms_700K_1GPa_Coefficients = [-11.52, -11.61, -11.42]
+Dissociation_Rate_50ms_700K_2GPa_Coefficients = [-21.35, -21.49, -21.21]
+Dissociation_Rate_50ms_700K_3GPa_Coefficients = [-33.7, -33.95, -33.46]
+Dissociation_Rate_50ms_700K_4GPa_Coefficients = [-38.96, -39.26, -38.66]
+Dissociation_Rate_50ms_700K_5GPa_Coefficients = [-51.19, -51.8, -50.58]
+
+
